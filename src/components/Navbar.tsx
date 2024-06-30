2b8b33158e412c8d2be03f5cc96716431920d4d7
DiegoA <diegoaneli@gmail.com>
2024-06-30 22:43:34 +0200

    text dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar = () => {
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <div className="text-2xl font-semibold text-gray-800">
           <Link href="/">
-            TaskManager
+            You4Task
           </Link>
         </div>
         <div className="flex space-x-6">
-          <Link href="#features" className="text-gray-600 hover:text-gray-800 transition duration-300">Features</Link>
-          <Link href="#testimonials" className="text-gray-600 hover:text-gray-800 transition duration-300">Testimonials</Link>
-          <Link href="#cta" className="text-gray-600 hover:text-gray-800 transition duration-300">Get Started</Link>
+          <Link href="#features" className="text-gray-600 hover:text-gray-800 transition duration-300">Caratteristiche</Link>
+          <Link href="#testimonials" className="text-gray-600 hover:text-gray-800 transition duration-300">Testimonianze</Link>
+          <Link href="#cta" className="text-gray-600 hover:text-gray-800 transition duration-300">Inizia Ora</Link>
         </div>
         <div>
           <Link href="/auth/signin">
