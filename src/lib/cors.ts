a0d71318913960b89f792b5b01935b0fde8af326
DiegoA <diegoaneli@gmail.com>
2024-08-22 13:36:12 +0200

    cors modify

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,10 +1,9 @@
-// /lib/cors.ts
 import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const cors = Cors({
   methods: ['GET', 'HEAD', 'POST'],
-  origin: 'http://localhost:3000', // Cambia questo con il tuo dominio
+  origin: 'http://localhost:3000', 
 });
 
 function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: { (req: Cors.CorsRequest, res: { statusCode?: number | undefined; setHeader(key: string, value: string): any; end(): any; }, next: (err?: any) => any): void; (arg0: any, arg1: any, arg2: (result: any) => void): void; }) {
